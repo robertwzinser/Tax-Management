01c5e1ed1d1925e3e83460f91afff66ee911b198
Zain Sheikh <zainsheikh327@gmail.com>
2024-09-22 19:55:47 -0400

    Dashboard page is updated and Profile page is created (rough draft, we await the completion of the database before seeing/testing the functionalities of everything so far)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignIn from './pages/SignIn';
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<Profile />} />
 
       </Routes>
     </Router>
